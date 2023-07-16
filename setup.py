684fbe31054092a9c746253e87fbc2e3cce14fa5
vib <124324107+vibOnTop@users.noreply.github.com>
2023-07-16 21:40:32 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='customnotifs',
+    version='1.0.0',
+    description='A library for creating colourful notifications.',
+    author='viben',
+    author_email='vibpredictor.licensing@gmail.com',
+    url='https://github.com/your-username/custom-messages',
+    py_modules=['custom_messages'],
+    install_requires=['colorama'],
+)
